aa21a73034434454df61fa2c0fd06c0bfb7f2f34
Manish Bisen <64102310+bisen-del@users.noreply.github.com>
2023-04-10 16:32:11 +0530

    Update BinarySet.java

--- BinarySet.java
+++ BinarySet.java
@@ -1,3 +1,22 @@
+// Suman is a laborious college boy and he is very good at mathematics. He is doing his assignment of computer science but he is not able to solve one coding problem.This problem consists of an integer, N and Suman has to find out the total number of set bits in all numbers starting from 1 to N.
+
+// Suman needs your help in solving the problem.
+
+// Input Format
+// First and only one line of the input contains one integer N.
+
+// Output Format
+// Print an integer - the total number of set bits in all the numbers starting  from 1 to N.
+
+// Constraints
+// 0<N<=10^7
+// Sample Testcase 0
+// Testcase Input
+// 5
+// Testcase Output
+// 7
+
+
 package com.company.JavaBasicProgram;
 
 public class BinarySet {
