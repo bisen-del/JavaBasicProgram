475aafd242285744fff1e2dce489572148e3caea
Manish Bisen <bisenmanu692@gmail.com>
2023-04-11 16:48:42 +0530

    add file

---
+++ NonZero.java
@@ -0,0 +1,26 @@
+package com.company.JavaBasicProgram;
+
+import java.util.Scanner;
+
+public class NonZero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int nonZeroIndex = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[nonZeroIndex];
+                arr[nonZeroIndex] = temp;
+                nonZeroIndex++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
