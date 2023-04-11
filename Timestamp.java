14ee23f99a1de936cc1e957ad4cc21e9869e4eac
Manish Bisen <64102310+bisen-del@users.noreply.github.com>
2023-04-11 12:54:07 +0530

    Update Timestamp.java

--- Timestamp.java
+++ Timestamp.java
@@ -1,6 +1,40 @@
-package com.company.JavaBasicProgram;
+// Steve is doing some competitive coding, but at any second ‘t’ when he made any submission, the previous submission, which was done 5000 seconds ago, will remove itself from the submission queue. He wants to calculate the maximum queue size he will get at any time ‘t’ after total 'n' submissions.
 
-import java.util.Scanner;
+// Help Steve to find the maximum size of queue.
+
+// Note: It is guaranteed that every submission uses a strictly larger value of ‘t’ than the previous submission.
+
+ 
+
+// Input Format
+// The first line of input consists of n– the size of array.
+
+// The second line consists of an array of n integers.
+
+// Output Format
+// Output a single line containing the maximum size possible.
+
+// Constraints
+// 1 <= n <= 10^5
+
+// 1 <= arr[i] <= 10^8
+
+ 
+
+// Sample Testcase 0
+// Testcase Input
+// 4
+// 1 100 5000 5002
+
+// Testcase Output
+// 3
+// Explanation
+// At an index 2 and 3 the queue size is maximum  i.e. 3. 
+
+
+// package com.company.JavaBasicProgram;
+
+// import java.util.Scanner;
 
 public class Timestamp {
     public static void main(String[] args) {
