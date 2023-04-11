4f32bac9dfe9ee2f8988be11a4d85fa118ec15c4
Manish Bisen <64102310+bisen-del@users.noreply.github.com>
2023-04-11 12:11:43 +0530

    Update Findkth.java

--- Findkth.java
+++ Findkth.java
@@ -1,3 +1,24 @@
+// Problem Statement
+// One day jack finds a string of characters. He is very keen to arrange the characters in reverse order, i.e., first characters become the last characters, second characters become the second-last characters, and so on. Now he wants your help  to find the kth character from the new string formed after reverse the original string.
+
+// Input Format
+// The first line contains two integer n, k — the length of array and kth character of the string respectively.
+// The second line contains a string containing n characters.
+// Output Format
+// Print the single line kth character of the string.
+
+// Constraints
+// 1 ≤ k ≤ n≤ 1000000
+// String contain only lowercase Latin letters.
+// Sample Testcase 0
+// Testcase Input
+// 5 2
+// abdfa
+// Testcase Output
+// f
+// Explanation
+// The kth character after the reversing the string is f.
+
 package com.company.JavaBasicProgram;
 
 public class Findkth {
