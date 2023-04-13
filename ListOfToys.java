bcb7a047f78d118c32699ce7b85a3c35741cb195
Manish Bisen <64102310+bisen-del@users.noreply.github.com>
2023-04-13 12:56:24 +0530

    Update ListOfToys.java

--- ListOfToys.java
+++ ListOfToys.java
@@ -1,3 +1,38 @@
+/* A toy manufacturing factory produced n toys on a day. The manager has a list of product IDs and another list containing the sizes of the corresponding toys of length n each.
+In other words, the ith integer in the product sizes list represents the size of the ith toy in the product ID list. He accidentally added the second list in front of the first
+one and now has a list of 2*n elements.
+
+ 
+
+You are an employee, and the manager wants you to recover the data and present it to him as another list of 2*n elements such that the product ID of each toy is followed by its size.
+
+Input Format
+The first line of the input contains a single integer n – the number of toys produced.
+
+The second contains 2*n space-separated integers a1, a2, …, an, b1, b2, …, bn – the merged list given to you by the manager.
+
+Output Format
+Print 2*n space-separated integers representing the list wanted by the manager
+
+Constraints
+1 <= n <= 105
+
+1 <= ai, bi<= 100
+
+Sample Testcase 0
+Testcase Input
+3
+2 5 1 3 4 7
+Testcase Output
+2 3 5 4 1 7 
+Sample Testcase 1
+Testcase Input
+4
+1 2 3 4 4 3 2 1
+Testcase Output
+1 4 2 3 3 2 4 1 
+*/
+
 package com.company.JavaBasicProgram;
 
 import java.util.Scanner;
